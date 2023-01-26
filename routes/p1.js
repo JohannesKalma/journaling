6f56e5b372a9c14f054e3568aaa206278382e3f4
Johannes Kalma <johannes@oarsnet.nl>
2023-01-26 23:13:10 +0100

    p1 met api

--- routes/p1.js
+++ routes/p1.js
@@ -2,8 +2,18 @@ const createError = require('http-errors');
 
 const express = require('express');
 const router = express.Router();
-
 const axios = require('axios');
+
+router.get('/api', async function(req, res, next) {
+    try {
+        const response = await axios.get('http://eyowv23r7po2py16.myfritz.net:10180/api/v1/smartmeter?limit=1&json=object');
+        console.log(response.data[0]);
+        res.json(response.data[0]);
+    } catch (error) {
+        console.error(error);
+        res.json({message:error});
+      }
+});
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
