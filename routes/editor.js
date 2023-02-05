5ba8853ed8ba5af560350adee41b3172d11a5b55
Johannes Kalma <johannes@oarsnet.nl>
2023-02-05 19:32:29 +0100

    remove legacy code

--- routes/editor.js
+++ routes/editor.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-var matter = require('gray-matter');
 var methodOverride = require('method-override')
 var router = express.Router();
-let fs = require('fs');
 const mongoose = require("mongoose");
 
 let jwt = require('jsonwebtoken');
@@ -59,17 +57,6 @@ router.get('/:id',async function(req,res,next){
   res.render('editor',{data:d,_method:'PUT'});
 });
 
-/* komt te vervallen */
-router.get('/legacy/:id', async function(req,res,next){
-  console.log('legacy route');
-  //edit document
-  var documentPath=__dirname + '/../documents/'+req.params.id;
-  let document = fs.readFileSync(documentPath,'utf-8');
-  doc = await matter(document);
-  //console.log(doc);
-  res.render('editor_legacy',{document:doc,_method:'PUT',file:req.params.id});
-});
-
 //router.use(bodyParser.urlencoded());
 router.use(methodOverride('_method'));
 
@@ -101,24 +88,5 @@ router.put('/:id', async function(req,res,next){
   });
   res.redirect(process.env.BASE_URL+'/'+req.params.id);
 });
-
-/* deze router gaat ooir vervallen */
-router.put('/legacy/:id',async function(req,res,next){
-  console.log('legacy put method');
-  let doc={};
-  doc['title']=req.body.Title;
-  doc['description']=req.body.Description;
-  doc['content']=req.body.content;
-  var model = await new Model({
-    title:req.body.Title,
-    description:req.body.Description,
-    content:req.body.content,
-    legacy: {filename:req.params.id},
-  });
-  // save the bear and check for errors
-  var result = await model.save();
-
-  res.redirect(process.env.BASE_URL+'/'+result._id.valueOf());
-})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
-let fs = require('fs');
 let jwt = require('jsonwebtoken');
-let matter = require('gray-matter');
 let createError = require('http-errors');
-const documents = require('../models/documents');
 
 const mongoose = require("mongoose");
 
@@ -15,17 +12,16 @@ let md = require('markdown-it')(
    breaks:true
   });
 
+const model = require("../models/documents") // new
 
-  const model = require("../models/documents") // new
+async function connectMongo() {
+    await mongoose.set("strictQuery", false);
+    await mongoose.connect('mongodb://127.0.0.1:27017/documents');
+    const db = await mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+}
 
-  async function connectMongo() {
-      await mongoose.set("strictQuery", false);
-      await mongoose.connect('mongodb://127.0.0.1:27017/documents');
-      const db = await mongoose.connection;
-      db.on("error", console.error.bind(console, "MongoDB connection error:"));
-  }
-  
-  connectMongo();
+connectMongo();
   
 router.use(function(req,res,next){
   if ( req.cookies.ACCESS_TOKEN ){
@@ -35,34 +31,10 @@ router.use(function(req,res,next){
   next();
 });
 
-let docPath=__dirname + '/../documents';
-
 /* GET - home page, show list of recipees */
 router.get('/', async function(req, res, next) {
   let documentHeaders = [];
   
-  /* start legacy */
-  let files = fs.readdirSync(docPath);
-  files.reverse;
-  //files.forEach(file => {
-  console.log('legacy get part - can be removed when all documents have been moved to mongo')
-  for await (f of files){ 
-    if (f.match('^[0-9]{8}-[0-9]{5}')){
-      let fileExists = await model.exists({legacy:{filename:f}});
-        //console.log( f + ' - ' + fileExists );
-        if (!fileExists){ 
-        let fileContent=fs.readFileSync(docPath+'/'+f,'utf8');
-        let document = matter(fileContent);
-        let documentHeader = {};
-        documentHeader.file = f;
-        documentHeader.data = document.data;
-        documentHeader.legacy = 'true';
-        documentHeaders.push(documentHeader);
-    }
-    } 
-  };
-  /* end legacy */
-
   /* mongo getter*/  
   var mongodocs = await model.find();
   for (doc of mongodocs){
@@ -78,17 +50,6 @@ router.get('/', async function(req, res, next) {
   }
   res.render('index',{documentHeaders:documentHeaders,access_granted:res.access_granted,title:'Journal Johannes'});
 });
-
-router.get('/legacy/:i',function(req, res, next) {
-  console.log ('old legacy');
-  if ( req.params.i.match('^[0-9]{8}-[0-9]{5}') ){
-      let fileContent=fs.readFileSync(docPath+'/'+req.params.i,'utf8');
-      dat=matter(fileContent);
-      //console.log(dat);
-      res.render('single_legacy',{file: req.params.i, data:dat.data, content: md.render(dat.content), access_granted:res.access_granted,title:dat.data.title});
-      
-  }
-})
 
 router.get('/:i',async function(req, res, next) {
       var d = await model.findById(req.params.i);
