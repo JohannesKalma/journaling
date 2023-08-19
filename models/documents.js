4bd538ac1534bd395c3558e06873077b8ea31ccb
Johannes Kalma <johannes@oarsnet.nl>
2023-08-19 17:36:26 +0200

    restanten

--- models/documents.js
+++ models/documents.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose")
 
 const RtSchema = mongoose.Schema({
-        title: { type:String, required:true},
-        description: String,
-        author: {type: String, default:'Johannes Kalma'},
-	    content: { type:String, required:true},
-        docType: { type: String,
-                   enum: ['recipe','document','collection'],
-                   default: 'document'
-                 },
-        journalType: {type: String}, 
-        legacy: {filename: {type: String, 
-                            index: { unique: true, sparse: true }},
-                 header: String
-                },
-        public: {type: Boolean, default:false},
+    title: { type: String, required: true },
+    description: String,
+    author: { type: String, default: 'Johannes Kalma' },
+    content: { type: String, required: true },
+    docType: {
+        type: String,
+        enum: ['recipe', 'document', 'collection'],
+        default: 'document'
     },
+    journalType: { type: String },
+    legacy: {
+        filename: {
+            type: String,
+            index: { unique: true, sparse: true }
+        },
+        header: String
+    },
+    public: { type: Boolean, default: false },
+},
     {
         timestamps: true,
     })
